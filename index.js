9e662e7054abf95dee67edd26f1470b2c472aea5
proxycrome <78900687+proxycrome@users.noreply.github.com>
2021-04-17 17:47:49 +0100

    This is the fileSystem task

--- index.js
+++ index.js
@@ -6,9 +6,23 @@ const url = "http://jsonplaceholder.typicode.com/posts";
 
 async function getPost(url){
     try{
-    const result = await fetch(url);
-    const jsonResponse = await result.json();
-    data = JSON.stringify(jsonResponse, null, 2);
+        const result = await fetch(url);
+        const jsonResponse = await result.json();
+        const data = JSON.stringify(jsonResponse, null, 2);
+
+        const server = http.createServer((request, response) => {
+            fs.writeFile('./result/posts.json', data , function(err){
+                if(err) throw err;
+                console.log("File written successfully");
+            });
+            response.writeHead(200);
+            response.end("File Created Successfully");
+        });
+        
+        server.listen(4000, () => {
+            console.log("Server is Running!");
+        });
+
     }catch(err){
         console.log(err);
     }
@@ -17,14 +31,4 @@ async function getPost(url){
 getPost(url);
 
 
-const server = http.createServer((request, response) => {
-    fs.writeFile('./result/posts.json', data , function(){
-        console.log("File written successfully");
-    });
-    response.writeHead(200);
-    response.end("File Created Successfully");
-})
 
-server.listen(4000, () => {
-    console.log("Server is Running!");
-})
